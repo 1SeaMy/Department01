efd7b052ccbca506b8c6262343c9b928e21516c8
1SeaMy <deniz.albayrak@gmail.com>
2023-12-27 23:29:04 +0300

    salary calculated

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -3,10 +3,15 @@ package org.example;
 public class Department {
     private int depId;
     private String depName;
+    private Employees employees;
 
-    public Department(int depId, String depName) {
+    public Department() {
+    }
+
+    public Department(int depId, String depName, Employees employees) {
         this.depId = depId;
         this.depName = depName;
+        this.employees = employees;
     }
 
     public int getDepId() {
@@ -24,74 +29,18 @@ public class Department {
     public void setDepName(String depName) {
         this.depName = depName;
     }
+
+    public Employees getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Employees employees) {
+        this.employees = employees;
+    }
+
     public void depDisplay() {
         System.out.println("-----DEPARTMAN  BİLGİLERİ-----");
         System.out.println("Departman Id : " + depId);
         System.out.println("Departman Adı : " + depName);
-    }
-
-    public static class Employess {
-        private int empId;
-        private String empName;
-        private String empSurname;
-        private String empPosition;
-        private double empSalary;
-
-        public Employess(int empId, String empName, String empSurname, String empPosition, double empSalary) {
-            this.empId = empId;
-            this.empName = empName;
-            this.empSurname = empSurname;
-            this.empPosition = empPosition;
-            this.empSalary = empSalary;
-        }
-
-        public int getEmpId() {
-            return empId;
-        }
-
-        public void setEmpId(int empId) {
-            this.empId = empId;
-        }
-
-        public String getEmpName() {
-            return empName;
-        }
-
-        public void setEmpName(String empName) {
-            this.empName = empName;
-        }
-
-        public String getEmpSurname() {
-            return empSurname;
-        }
-
-        public void setEmpSurname(String empSurname) {
-            this.empSurname = empSurname;
-        }
-
-        public String getEmpPosition() {
-            return empPosition;
-        }
-
-        public void setEmpPosition(String empPosition) {
-            this.empPosition = empPosition;
-        }
-
-        public double getEmpSalary() {
-            return empSalary;
-        }
-
-        public void setEmpSalary(double empSalary) {
-            this.empSalary = empSalary;
-        }
-
-        public void empDisplay() {
-            System.out.println("-----PERSONEL BİLGİLERİ-----");
-            System.out.println("Personel Id : " + empId);
-            System.out.println("Personel Adı : " + empName);
-            System.out.println("Personel Soyadı : " + empSurname);
-            System.out.println("Personel Pozisyonu : " + empPosition);
-            System.out.println("Personel Maaşı : " + empSalary);
-        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,37 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Department department01=new Department(1, "Muhasebe");
-        Department.Employess employees01=new Department.Employess(1, "Ayşe", "Can", "Muhasebe Müdürü", 50000);
+        Employees employees01=new Employees(1, "Ayşe", "Can", "Muhasebe Müdürü", 25, 55000);
+        Employees employees02=new Employees(2, "Ali", "Kara", "Muhasebe Uzmanı", 30,45000);
+
+        Department department01=new Department(1, "Muhasebe",employees02);
 
         department01.depDisplay();
+        System.out.println("########################################################");
         employees01.empDisplay();
-        System.out.println("------------------------------");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Güncellenecek veri : "+ employees01.getEmpId() + ". personelin Soyadı: " + employees01.getEmpSurname());
         employees01.setEmpSurname("Canan");
-        System.out.println("Güncellenen veri :\n "+ employees01.getEmpId() + ". personelin Soyadı: " +employees01.getEmpSurname());
-
+        System.out.println("Güncellenen veri   : "+ employees01.getEmpId() + ". personelin Soyadı: " + employees01.getEmpSurname());
+        System.out.println("--------------------------------------------------------");
+        employees01.empDisplay();
+        System.out.println("########################################################");
+        department01.getEmployees().empDisplay();
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Güncellenecek veri : "+ department01.getEmployees().getEmpId() + ". personelin Maaşı: " + department01.getEmployees().getEmpSalary());
+        department01.getEmployees().setEmpSalary(51000);
+        System.out.println("Güncellenen veri   : "+ department01.getEmployees().getEmpId() + ". personelin Maaşı: " + department01.getEmployees().getEmpSalary());
+        System.out.println("--------------------------------------------------------");
+        department01.getEmployees().empDisplay();
+        System.out.println("########################################################");
+        department01.setEmployees(employees01);
+        department01.getEmployees().empDisplay();
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Güncellenecek veri : "+ department01.getEmployees().getEmpId() + ". personelin Maaşı: " + department01.getEmployees().getEmpSalary());
+        department01.getEmployees().setEmpSalary(61000);
+        System.out.println("Güncellenen veri   : "+ department01.getEmployees().getEmpId() + ". personelin Maaşı: " + department01.getEmployees().getEmpSalary());
+        System.out.println("--------------------------------------------------------");
+        department01.getEmployees().empDisplay();
+        System.out.println("--------------------------------------------------------");
     }
 }
